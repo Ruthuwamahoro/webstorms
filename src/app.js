ff08a2aa934d36b7dd01cf087db6fa6e304ccf1f
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2026-09-03 20:33:31 +0200

    formatting exLint and prettier

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import express from "express";
+import express from 'express';
 const app = express();
 
-app.get("/", (re, res) => {
-    res.status(200).send('Hello from Acquisations')
-})
+app.get('/', (re, res) => {
+  res.status(200).send('Hello from Acquisations');
+});
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import app from "./app.js";
-const PORT = process.env.PORT || 3000
+import app from './app.js';
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is listening to Port http://localhost:${PORT}`)
-})
+  console.log(`Server is listening to Port http://localhost:${PORT}`);
+});
